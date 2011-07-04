a5a0dd16bb44fe3ab9ec4a26f330f610bf1fe883
migcosta <migcosta@b98d7034-be06-3134-cd59-1147d85ee411>
2011-07-04 10:53:56 +0000

    New class for inner class that was not generated by compiler in a machine.

--- PwaLucene/src/main/java/org/apache/lucene/search/PwaPositionsManager.java
+++ PwaLucene/src/main/java/org/apache/lucene/search/PwaPositionsManager.java
@@ -175,25 +175,3 @@ public class PwaPositionsManager {
 		return field;
 	}
 }
-
-
-/**	 
- * PriorityQueue
- */
-class PwaPhraseQueue extends PriorityQueue {
-
-	/**
-	 * Constructor
-	 * @param size queue size
-	 */
-	public PwaPhraseQueue(int size) {
-		initialize(size);
-	}
-
-	/**
-	 * Comparator
-	 */
-	protected final boolean lessThan(Object o1, Object o2) {		
-		return ((PwaPositions)o1).get()<((PwaPositions)o2).get();
-	}
-}
