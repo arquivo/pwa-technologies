343b207109aded156fdfd1002d0f114340d126f1
migcosta <migcosta@b98d7034-be06-3134-cd59-1147d85ee411>
2014-03-17 18:29:23 +0000

    UrlNormalizer changed. Now it ignores dates with 00 days and months.

--- PwaArchive-access/projects/nutchwax/nutchwax-thirdparty/nutch/src/java/org/apache/nutch/util/UrlNormalizer.java
+++ PwaArchive-access/projects/nutchwax/nutchwax-thirdparty/nutch/src/java/org/apache/nutch/util/UrlNormalizer.java
@@ -6,6 +6,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.hadoop.util.ToolBase;
 import org.apache.nutch.net.URLNormalizers;
@@ -45,7 +48,7 @@ public class UrlNormalizer extends ToolBase {
 	public int run(String[] args) throws Exception {
 	    							
 		int nFields=Integer.parseInt(args[2]);
-		int indexField=Integer.parseInt(args[3]);
+		int indexField=Integer.parseInt(args[3]);		
 		
 		System.out.println("Start normalizing "+args[0]+" file.");
 				
@@ -55,21 +58,26 @@ public class UrlNormalizer extends ToolBase {
 		String parts[]=null;
 		StringBuffer sbuf=null;
 		StringBuffer surl=null;
-//		StringBuffer lastKey=null; // to eliminate duplicated keys: date + url normalized
-//		StringBuffer key=null;
+
 		while ( ( line = br.readLine() ) != null ) {				
 			parts = line.split(TOKEN);
 		
+			/*
 			if (parts.length!=nFields) { 
-				//throw new IOException("ERROR: wrong line:"+line);
-				//System.out.println("Possible wrong line:"+line);
+				throw new IOException("ERROR: wrong line:"+line);
+				System.out.println("Possible wrong line:"+line);
 			}	
+			*/
 			
-//			key=new StringBuffer(500);
 			sbuf=new StringBuffer(500);
-			surl=new StringBuffer(500);
+			surl=new StringBuffer(500);			
 			try {
 				for (int i=0;i<parts.length;i++) {
+										
+					if (i==0) { // check data
+						
+					}
+					
 					if (i>=indexField && i<=parts.length-nFields+indexField) {
 						if (i!=0) { 
 							if (i==indexField) {
@@ -82,27 +90,41 @@ public class UrlNormalizer extends ToolBase {
 						surl.append(parts[i]);
 						if (i==parts.length-nFields+indexField) {
 							sbuf.append("\""+normalize(surl.substring(1,surl.length()-1))+"\"");
-//							key.append("\""+normalize(surl.substring(1,surl.length()-1))+"\""); // append url normalized
 						}
 					}
 					else {
-						if (i!=0) { 
-							sbuf.append(TOKEN);						
+						if (i==0) {							
+							// check date
+							String sdate=parts[0].substring(1,parts[0].length()-1);						
+							SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");	
+							try {
+								Date date = sdf.parse(sdate); // throw ParseException
+							}
+							catch (ParseException e) {				
+								sdate+="00"; // for errors such as these "2000-11-19 16:28:"
+								Date date = sdf.parse(sdate); // throw ParseException							
+							}								
+							if (sdate.indexOf("-00")!=-1) { // for errors such as these - date/time field value out of range: "2000-00-10 01:23:52"
+								throw new ParseException("Wrong date in this version: "+line, 0);
+							}
+							
+							sbuf.append("\""+sdate+"\"");
 						}
-						else {
-//							key.append(parts[i]).append(TOKEN); // append time
-						}
-						sbuf.append(parts[i]);
-					}					
+						else {						
+							sbuf.append(TOKEN);
+							sbuf.append(parts[i]);
+						}						
+					}				
 				}	
-//				if (lastKey==null || !key.toString().equals(lastKey.toString())) {
+
 				pw.println(sbuf);
-//				}
-//				lastKey=key;
 			}
 			catch (MalformedURLException e) {
 				System.out.println("MalformedURLException for url "+surl.substring(1,surl.length()-1));
-			}																				
+			}					
+			catch (ParseException e) {				
+				System.out.println("Wrong date in this version: "+line);
+			}		
 		}
 		br.close();
 		pw.close();
