289a634db1e2c680bc00f1dc8866c723b91c4df3
hugohaviana@gmail.com <hugohaviana@gmail.com@b98d7034-be06-3134-cd59-1147d85ee411>
2014-11-28 17:35:39 +0000

    Added feature to check all Urls on blacklist kept hidden.

--- scripts/Ruby_Scripts/BlackList/buildBlacklist.rb
+++ scripts/Ruby_Scripts/BlackList/buildBlacklist.rb
@@ -1,18 +1,46 @@
+#!/usr/bin/env ruby
 #
 #Developed by PWA
 #This is the mais class to execute the Blacklist
 #
 #
+def checkUrls
+if CheckURLs.eql? "V"
+    #Target.write("URL Number\n")
+    targetWrite = open(FileName, 'r')
+   
+    targetWrite.each_line do |line|
+        num = line.split(" ").last.to_i
+        url_aux = line.split(" ").first
+        InfoUrl.each do|key,value|
+            if key.eql? url_aux
+                unless value.to_i == num
+                    puts "URL Changes on blacklist #{key} #{value}"
+                end
+            end
+        end
+    end
+end
+end
+FileName= '/shareT2/backups/configs/httpd/Brookers/URLsInfo' # This is the name of the file where is stored information about number of url that was gathered
+Target = open(FileName, 'w')
+
+CheckURLs = ARGV[0]
 require_relative 'ParseTabelaPrincipal'
 PWA = ParseTabelaPrincipal.new
 nUrls=0
 PWA.clearFiles
+InfoUrl = Hash.new
 File.open($FileName).each do |url|
 churl = "http://arquivo.pt/search.jsp?l=pt&query="+url
 PWA.Initialize(churl)
 link_arr_1=PWA.ParsePage('a',"<a href=\"http://arquivo.pt/")
+InfoUrl[url.gsub("\n","")] = link_arr_1.length
 puts url
 puts "Number of URL's: #{link_arr_1.length}"
+if CheckURLs.eql? "V"
+    Target.write("#{url.gsub("\n","")} #{link_arr_1.length}\n")
+end
 nUrls+=link_arr_1.length
 if link_arr_1.length >0
     PWA.printUrlWithID(link_arr_1,url)
@@ -22,4 +50,7 @@ else
     PWA.printBrokenLinks
 end
 end
+Target.close
 puts "Total Number of URLs #{nUrls}"
+checkUrls
+#puts InfoUrl
