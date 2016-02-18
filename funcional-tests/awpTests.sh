df83bbfdbf41b0bbb9f428477c63678c64a12682
FCCNhviana <hugo.viana@fccn.pt>
2016-02-18 15:56:18 +0000

    functional tests path updated

--- funcional-tests/awpTests.sh
+++ funcional-tests/awpTests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DISPLAY=:0
-cd /root/awp-platatests
+cd /root/pwa-technologies/functional-tests/
 plataResult=`/usr/bin/ant test -Dtestcase=pt.fccn.arquivo.tests.AllTests -Dtest.url=http://arquivo.pt  | grep "Failures: 0, Errors: 0"`
 
 if [ -z "$plataResult" ]; then
