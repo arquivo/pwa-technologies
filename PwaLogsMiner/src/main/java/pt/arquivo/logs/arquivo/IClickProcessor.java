61d9c83baf7f17808d4212e2c5805b6ff24da627
FCCNhviana <hugo.viana@fccn.pt>
2016-05-23 15:29:53 +0100

    LogsMiner removed

--- PwaLogsMiner/src/main/java/pt/arquivo/logs/arquivo/IClickProcessor.java
+++
@@ -1,9 +0,0 @@
-package pt.arquivo.logs.arquivo;
-
-/**
- * Process clicks
- * @author Miguel Costa
- */
-public interface IClickProcessor {
-	ClickEntry exec(String queryPartsField0, String queryPartsField1);
-}
